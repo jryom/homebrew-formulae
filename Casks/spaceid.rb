7cdbf843851795ed87acb795179b6e0db255deb0
Jesper Ryom <jesper@ryom.me>
2020-09-11 15:31:06 +0200

    update spaceid

--- Casks/spaceid.rb
+++ Casks/spaceid.rb
@@ -1,17 +1,13 @@
 cask 'spaceid' do
-  version '1.3'
-  sha256 '3df4440e9ef96c8408342297a7a287be931976c655a3ec772a96c8082f3a7a81'
+  version '1.3.1'
+  sha256 'a4e467ac24322c706213fb0134832523bdd73e0b66471756e342eb796a732e4e'
 
-  url "https://github.com/dshnkao/SpaceId/releases/download/v#{version}/SpaceId.app.zip"
+  url "https://github.com/dshnkao/SpaceId/releases/download/#{version}/SpaceId.app.zip"
   appcast 'https://github.com/dshnkao/SpaceId/releases.atom'
   name 'SpaceId'
   homepage 'https://github.com/dshnkao/SpaceId/'
 
   app 'SpaceId.app'
-
-  preflight do
-    set_permissions "#{staged_path}/SpaceId.app", '0755'
-  end
 
   uninstall login_item: 'SpaceId'
 
